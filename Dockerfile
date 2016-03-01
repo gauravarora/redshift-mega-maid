bcbe3da54312acede209a30808ce3dcdd91f12ca
James Cuzella <james.cuzella@returnpath.com>
2016-03-01 10:29:42 -0700

    Make this code open source, but ensure Amazon's License is mentioned
    Change-Id: Id0b4ebdcdb6bce1dd95e8b1ae7bac1c5c53c4732

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,28 @@
+#
+#                                 Apache License
+#   Copyright 2016 ReturnPath
+#   Copyright 2016 James Cuzella
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##
+## Note: This Docker container and script only automate installation & wrap
+##       the original tools released as: "awslabs/amazon-redshift-utils"
+##       The source code distributed in this repo is under the Apache License,
+##       while the source code provided by Amazon is distributed under the
+##       "Amazon Software License": http:aws.amazon.com/asl/
+
+
 FROM centos:centos7
 MAINTAINER ReturnPath EFP Core <@ReturnPath>
 
--- bin/analyze-vacuum-schema.sh
+++ bin/analyze-vacuum-schema.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#
+#                                 Apache License
+#   Copyright 2016 ReturnPath
+#   Copyright 2016 James Cuzella
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+##
+## Note: This Docker container and script only automate installation & wrap
+##       the original tools released as: "awslabs/amazon-redshift-utils"
+##       The source code distributed in this repo is under the Apache License,
+##       while the source code provided by Amazon is distributed under the
+##       "Amazon Software License": http:aws.amazon.com/asl/
 
 python /opt/amazon-redshift-utils/src/AnalyzeVacuumUtility/analyze-vacuum-schema.py \
        --db $MM_DB_NAME --db-user $MM_DB_USER --db-pwd $MM_DB_PASS \
